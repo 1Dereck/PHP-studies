d7ce9f20e952a2f6661b92145c31504b802b3e86
Dereck Felipe Maciel Pereira <pedrequewr@gmail.com>
2026-03-25 10:13:13 -0300

    comit

---
+++ 03-strings-and-arrays/Collections Strings/arrays.php
@@ -0,0 +1,11 @@
+<?php
+
+$array = [
+    0 => "um",
+    1 => "dois",
+    2 => "tres"
+];
+
+foreach ($array as $numeral => $nomeNumero) {
+    echo "$numeral em portugês é: $nomeNumero\n";
+}
---
+++ 03-strings-and-arrays/Collections Strings/numero.php
@@ -0,0 +1,7 @@
+<?php
+
+$anoNascimento = "2004";
+
+$idade = 2026 - $anoNascimento;
+
+echo $idade . PHP_EOL;
