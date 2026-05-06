debfcf735aa8fcdfdf8a23db481c0bd5d51c04a9
Dereck Felipe Maciel Pereira <pedrequewr@gmail.com>
2026-05-06 13:40:09 -0300

    Use string key for route lookup
    Fix route key construction in php-mvc/public/index.php by replacing the incorrect bitwise/array expression with a string "$httpMethod|$pathInfo". Also use the same string when indexing $routes so controllers are resolved correctly instead of using a malformed key.

--- php-mvc/public/index.php
+++ php-mvc/public/index.php
@@ -15,10 +15,10 @@ $routes = require_once __DIR__ . '/../config/routes.php';
 $pathInfo = $_SERVER['PATH_INFO'] ?? '/';
 $httpMethod = $_SERVER['REQUEST_METHOD'];
 
-$key = [$httpMethod | $pathInfo];
+$key = "$httpMethod|$pathInfo";
 
 if (array_key_exists($key, $routes)) {
-    $controllerClass = $routes[$httpMethod | $pathInfo];
+    $controllerClass = $routes["$httpMethod|$pathInfo"];
     $controller = new $controllerClass($videoRepository);
 } else {
     $controller = new Error404Controller();
